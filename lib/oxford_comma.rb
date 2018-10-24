13d6dfc656f7810fdc1215f87d9eb21e0a137f6a
John Martinez <jmartinez729@gmail.com>
2018-10-24 18:44:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.length == 1
-    return array.join(", ")
+    return array.join
   elsif array.length == 2
     array[-1] = "and #{array[-1]}"
     return array.join(" ")
